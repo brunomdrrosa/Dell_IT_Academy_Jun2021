93fb574aa9b4b1d1550d2baa4447677e72f7787b
Bruno Machado <brunomdr46@gmail.com>
2021-06-10 18:44:06 -0300

    Opção 2

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ lista_numero = dados["numero"].to_list()
 lista_latitude = dados["latitude"].to_list()
 lista_longitude = dados["longitude"].to_list()
 
+# Função para armazenar a latitude e longitude do usuário
+def opcao2():
+    print("Informe sua localização:")
+    latitude = float(input("Digite sua latitude: "))
+    longitude = float(input("Digite sua longitude: "))
+    print("Localização armazenada.")
+    menu()
+
 # Menu do programa
 def menu():
     print()
@@ -23,16 +31,21 @@ def menu():
     print("4. Buscar pontos por logradouro")
     print("5. Terminar o programa")
     opcao = int(input("Escolha uma das opcões:\n"))
+
     # Listar na tela os dados de todos os pontos de taxi da cidade
     if opcao == 1:
         print(dados)
         menu()
+    # Permitir que o usuário digite sua localização geográfica e armazená-la
     elif opcao == 2:
-        print("Opção 2")
+        opcao2()
+    # Encontrar os 3 pontos mais próximos baseado na latitude e longitude do usuário
     elif opcao == 3:
-        print("Opção 3")
+        print()
+    # Buscar pontos por logradouro
     elif opcao == 4:
         print("Opção 4")
+    # Fechar o programa
     elif opcao == 5:
         print("Fechando o programa...")
     else:
